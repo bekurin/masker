75dc7ce4b564eedfee74ce2f02da3886ad570d61
bekurin <bekurin17@gmail.com>
2023-04-15 18:26:18 +0900

    initial commit

---
+++ core/build.gradle.kts
@@ -0,0 +1,34 @@
+val kotlinStdlib: String by project
+val junitJupiterParams: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinStdlib")
+
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterParams")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
